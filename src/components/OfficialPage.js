a3ea6bcb45e0f1f1809b0e0ef3fd9e7797fc721a
Skillz <gankstarsleague@gmail.com>
2019-02-04 22:57:14 -0500

    more fixes to official page

--- src/components/OfficialPage.js
+++ src/components/OfficialPage.js
@@ -1,86 +1,61 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { TwitterTimelineEmbed } from 'react-twitter-embed';
 import Card from './Card';
 import Carousel from './Carousel';
-
-const tileData = [
-  {
-    img:
-      'https://cdn.discordapp.com/attachments/530862523562328083/542090576489742336/Ylva_splash.jpg',
-    title: '2018 VPL WORLD INVITATIONAL PRESENTED BY RAZER PHONE 2',
-    author: 'BY: READYPLAYER1',
-    link:
-      'https://www.vainglorygame.com/news/2018-vpl-world-invitational-presented-razer-phone-2/',
-  },
-  {
-    img:
-      'https://cdn.discordapp.com/attachments/530862523562328083/542090625760100382/Magnus_Standard.jpg',
-    title: 'UPDATE 3.9: MAGNUS THE ARCANE PRINCE',
-    author: 'BY: SPACEMONKEY',
-    link: 'https://www.vainglorygame.com/news/update-3-9-magnus-arcane-prince/',
-  },
-  {
-    img:
-      'https://jd3sljkvzi-flywheel.netdna-ssl.com/wp-content/uploads/2018/12/balancenotes_article-1.jpg',
-    title: 'UPDATE 3.9 HERO & ITEM BALANCE CHANGES',
-    author: 'By: SPACEMONKEY',
-    link:
-      'https://www.vainglorygame.com/news/update-3-9-hero-item-balance-changes/',
-  },
-  {
-    img:
-      'https://jd3sljkvzi-flywheel.netdna-ssl.com/wp-content/uploads/2018/12/battlepass_article.jpg',
-    title: '3.9 WINTER BATTLE PASS: STAYING LIT',
-    author: 'BY: SPACEMONKEY',
-    link:
-      'https://www.vainglorygame.com/news/3-9-winter-battle-pass-staying-lit/',
-  },
-  {
-    img:
-      'https://jd3sljkvzi-flywheel.netdna-ssl.com/wp-content/uploads/2018/11/article_midpatchupdate-1.jpg',
-    title: 'MID-UPDATE BALANCE REALIGNMENT',
-    author: 'BY: WOLF_HANDS',
-    link: 'https://www.vainglorygame.com/news/38midupdatebalance/',
-  },
-  {
-    img:
-      'https://jd3sljkvzi-flywheel.netdna-ssl.com/wp-content/uploads/2018/11/article.jpg',
-    title: 'SUNLIGHT FOR DARKENING DAYS',
-    author: 'BY: READYPLAYER1',
-    link: 'https://www.vainglorygame.com/news/sunlight-darkening-days/',
-  },
-];
+import Parser from 'rss-parser';
 
 const twitterAccounts = ['vainglory', 'vainglorystatus', 'vaingloryesport'];
 
-export default () => (
-  <div>
-    <Carousel />
-    <div className="CardboxGroup">
-      {tileData.map((tile, index) => (
-        <Card
-          link={tile.link}
-          image={tile.img}
-          title={tile.title}
-          text={tile.author}
-          key={index}
-        />
-      ))}
-    </div>
-    <br />
-    <br />
-    <br />
-    {twitterAccounts.map((account) => (
-      <TwitterTimelineEmbed
-        sourceType="profile"
-        screenName={account}
-        theme="dark"
-        noHeader
-        noFooter
-        noScrollbar
-        options={{ height: 400 }}
-        key={account}
-      />
-    ))}
-  </div>
-);
+export default class OfficialPage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { news: [] };
+  }
+
+  async componentDidMount() {
+    // Note: some RSS feeds can't be loaded in the browser due to CORS security.
+    // To get around this, you can use a proxy.
+    const CORS_PROXY = 'https://cors-anywhere.herokuapp.com/';
+    const parser = new Parser();
+    const feed = await parser.parseURL(
+      CORS_PROXY + 'https://www.vainglorygame.com/feed/'
+    );
+
+    this.setState({
+      news: feed.items.map((item) => ({
+        author: `BY: ${item.creator.toUpperCase()} On ${item.pubDate.substring(
+          0,
+          item.pubDate.length - 15
+        )}`,
+        title: item.title.toUpperCase(),
+        link: item.link,
+        img:
+          item['content:encoded'].indexOf('src="') >= 0
+            ? item['content:encoded'].substring(
+                item['content:encoded'].indexOf('src="') + 5,
+                item['content:encoded'].indexOf('alt="') - 2
+              )
+            : 'https://thumbs.gfycat.com/SickTepidIndigowingedparrot-small.gif',
+      })),
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <Carousel news={this.state.news.splice(0, 3)} />
+        <div className="CardboxGroup">
+          {this.state.news.map((tile, index) => (
+            <Card
+              link={tile.link}
+              image={tile.img}
+              title={tile.title}
+              text={tile.author}
+              key={index}
+            />
+          ))}
+        </div>
+      </div>
+    );
+  }
+}
