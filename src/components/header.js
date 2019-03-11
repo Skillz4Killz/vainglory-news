cae1f1e13f2ef67aa93970ccaf64f9e44b567753
Angelo Cantone <angelocantone@imac-di-angelo.homenet.telecomitalia.it>
2019-03-11 16:38:50 +0100

    Header Icons

--- src/components/header.js
+++ src/components/header.js
@@ -1,6 +1,7 @@
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
+import './header.css'
 
 const Header = ({ siteTitle }) => (
   <header>
@@ -19,28 +20,28 @@ const Header = ({ siteTitle }) => (
         }}
       >
         <li>
-          <Link to="/">
-            <i className="fas fa-newspaper navicon" /> Latest
+          <Link to="/" className='tab'>
+            <img src={require('./../images/VGNews.png')} className="fas fa-newspaper navicon" /> Latest
           </Link>
         </li>
         <li>
-          <Link to="/art">
-            <i className="fas fa-palette navicon" /> Art
+          <Link to="/art" className='tab'>
+            <img src={require('./../images/VGNews.png')} className="fas fa-palette navicon" /> Art
           </Link>
         </li>
         <li>
-          <Link to="/guides">
-            <i className="fas fa-lightbulb navicon" /> Guides
+          <Link to="/guides" className='tab'>
+            <img src={require('./../images/VGNews.png')} className="fas fa-lightbulb navicon" /> Guides
           </Link>
         </li>
         <li>
-          <Link to="/esports">
-            <i className="fas fa-trophy navicon" /> Esports
+          <Link to="/esports" className='tab'>
+            <img src={require('./../images/VGNews.png')} className="fas fa-trophy navicon" /> Esports
           </Link>
         </li>
         <li>
-          <Link to="/tools">
-            <i className="fas fa-tools navicon" /> Tools
+          <Link to="/tools" className='tab'>
+            <img src={require('./../images/VGNews.png')} className="fas fa-tools navicon" /> Tools
           </Link>
         </li>
       </ul>
