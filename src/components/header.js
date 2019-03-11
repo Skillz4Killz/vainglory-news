97684f2908ffa88c84d3eedd1c8fc2909a07fbd1
Skillz <gankstarsleague@gmail.com>
2019-03-11 12:20:46 -0400

    adding the navbar icons

--- src/components/header.js
+++ src/components/header.js
@@ -1,7 +1,7 @@
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
-import './header.css'
+import "./header.css"
 
 const Header = ({ siteTitle }) => (
   <header>
@@ -20,28 +20,44 @@ const Header = ({ siteTitle }) => (
         }}
       >
         <li>
-          <Link to="/" className='tab'>
-            <img src={require('./../images/VGNews.png')} className="fas fa-newspaper navicon" /> Latest
+          <Link to="/" className="tab">
+            <img
+              src={require("./../images/newspaper-solid.svg")}
+              className="navbarIcons"
+            />{" "}
+            Latest
           </Link>
         </li>
         <li>
-          <Link to="/art" className='tab'>
-            <img src={require('./../images/VGNews.png')} className="fas fa-palette navicon" /> Art
+          <Link to="/art" className="tab">
+            <img src={require("./../images/palette-solid.svg")} /> Art
           </Link>
         </li>
         <li>
-          <Link to="/guides" className='tab'>
-            <img src={require('./../images/VGNews.png')} className="fas fa-lightbulb navicon" /> Guides
+          <Link to="/guides" className="tab">
+            <img
+              src={require("./../images/lightbulb-solid.svg")}
+              className="fas fa-lightbulb navicon"
+            />{" "}
+            Guides
           </Link>
         </li>
         <li>
-          <Link to="/esports" className='tab'>
-            <img src={require('./../images/VGNews.png')} className="fas fa-trophy navicon" /> Esports
+          <Link to="/esports" className="tab">
+            <img
+              src={require("./../images/trophy-solid.svg")}
+              className="fas fa-trophy navicon"
+            />{" "}
+            Esports
           </Link>
         </li>
         <li>
-          <Link to="/tools" className='tab'>
-            <img src={require('./../images/VGNews.png')} className="fas fa-tools navicon" /> Tools
+          <Link to="/tools" className="tab">
+            <img
+              src={require("./../images/tools-solid.svg")}
+              className="fas fa-tools navicon"
+            />{" "}
+            Tools
           </Link>
         </li>
       </ul>
