8637550ee9bd326eae171e22f7815b6dfe3f272a
Skillz <gankstarsleague@gmail.com>
2019-04-20 17:57:52 -0400

    more text fixes to the card css

--- src/components/Card.js
+++ src/components/Card.js
@@ -7,10 +7,8 @@ export default props => (
       <a href={props.link}>
         <img className='Image2' src={props.image} alt={props.text} />
         <img src={props.image} alt={props.text} />
-        <h4 className={props.art ? "artTitle" : ""}>{props.title}</h4>
-        <p style={props.art ? { top: "75px", marginLeft: "35%" } : {}}>
-          {props.text}
-        </p>
+        <h4 className={props.art ? "artTitle" : "title"}>{props.title}</h4>
+        <p className={props.official ? "officialAuthor" : "author"}>{props.text}</p>
       </a>
     </div>
   </span>
