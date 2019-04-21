0521f75c234b9a53f5e5cff48c587dd6d887bdc7
Skillz <gankstarsleague@gmail.com>
2019-04-20 22:41:51 -0400

    making the website a mobile app on homescreen

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,12 +18,12 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `Vainglory News`,
+        short_name: `VGNews`,
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
-        display: `minimal-ui`,
+        display: `fullscreen`,
         icon: `src/images/VGNews.png`, // This path is relative to the root of the site.
       },
     },
