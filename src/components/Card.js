290067346c0f756e77434009c61ab76330cb2562
Skillz <gankstarsleague@gmail.com>
2019-05-04 10:04:51 -0400

    more stuff

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,27 +1,40 @@
 import React from "react"
 import "./Card.css"
-import GatsbyImage from 'gatsby-image'
+import GatsbyImage from "gatsby-image"
+import styled from "styled-components"
 
-/*
-const imageNode = data.allMongodbProdPosts.edges.find(edge => edge.node.image === props.image)
-if (!imageNode.node.localImage.childImageSharp) console.log(imageNode, imageNode.node.id, props.image)
-const imageData = imageNode.node.localImage.childImageSharp.fluid
-*/
+const StyledGatsbyImage = styled(GatsbyImage)`
+  position: absolute;
+  top: 0%;
+  left: 50%;
+  transform: translateX(-50%) translateY(-0%);
+  width: 100%;
+  height: 100%;
+`
 
-export default (props) => (
+export default props => (
   <a href={props.link}>
     <div className="Card">
       {/*props.fixed
         ? (<GatsbyImage className="image2" fixed={props.fixed} />)
         : (<img className="image2" src={props.image} alt={props.title} />)
       */}
-      {props.fixed
-        ? (<GatsbyImage className="Image1" fixed={props.fixed} />)
-        : (<img className="Image1" src={props.image} alt={props.title} />)
-      }
-      <div className='gradient'></div>
-      <h4 className={props.art ? "artTitle" : "title"}>{props.official ? props.title : props.title ? props.title.substring(0, 30) : ''}</h4>
-      <p className={props.official ? "officialAuthor" : "author"}>{props.text}</p>
+      {props.fixed ? (
+        <StyledGatsbyImage className="Image1" fixed={props.fixed} />
+      ) : (
+        <img className="Image1" src={props.image} alt={props.title} />
+      )}
+      <div className="gradient" />
+      <h4 className={props.art ? "artTitle" : "title"}>
+        {props.official
+          ? props.title
+          : props.title
+          ? props.title.substring(0, 30)
+          : ""}
+      </h4>
+      <p className={props.official ? "officialAuthor" : "author"}>
+        {props.text}
+      </p>
     </div>
   </a>
 )
--- src/templates/news-pages.js
+++ src/templates/news-pages.js
@@ -7,70 +7,84 @@ import SEO from "../components/seo"
 export default ({ pageContext }) => (
   <StaticQuery
     query={graphql`
-		query NewsPages {
-			allMongodbProdPosts {
-				edges {
-					node {
-						localImage {
-							childImageSharp {
-								fixed {
-									...GatsbyImageSharpFixed
-								}
-							}
-						}
-						id
-						path
-						author
-						category
-						image
-						link
-						messageID
-						channelID
-						title
-						stream
-					}
-				}
-			}
-		}
+      query NewsPages {
+        allMongodbProdPosts {
+          edges {
+            node {
+              localImage {
+                childImageSharp {
+                  fixed {
+                    ...GatsbyImageSharpFixed
+                  }
+                }
+              }
+              id
+              path
+              author
+              category
+              image
+              link
+              messageID
+              channelID
+              title
+              stream
+            }
+          }
+        }
+      }
     `}
     render={data => {
       const feed = Object.values(pageContext)
       const category = feed[0].category
-      if (!category) return null;
-      const relevantData = data.allMongodbProdPosts.edges.filter(edge => edge.node.category === category);
-      if (category === 'tools') console.log(relevantData[0])
+      if (!category) return null
+      const relevantData = data.allMongodbProdPosts.edges.filter(
+        edge => edge.node.category === category
+      )
 
       return (
         <Layout>
-          <SEO title="Latest" keywords={[`vainglory`, `news`, `skillz4killz`]} />
+          <SEO
+            title="Latest"
+            keywords={[`vainglory`, `news`, `skillz4killz`]}
+          />
           <div className="CardboxGroup">
             {category === "tools"
               ? relevantData
-                .map(a => ({ sort: Math.random(), value: a }))
-                .sort((a, b) => a.sort - b.sort)
-                .map((tool, index) => (
-                  <Card
-                    link={tool.value.node.link}
-                    fixed={tool.value.node.localImage.childImageSharp ? tool.value.node.localImage.childImageSharp.fixed : null}
-                    image={tool.value.node.image}
-                    title={tool.value.node.name}
-                    text={tool.value.node.description}
-                    key={index}
-                  />
-                ))
+                  .map(a => ({ sort: Math.random(), value: a }))
+                  .sort((a, b) => a.sort - b.sort)
+                  .map((tool, index) => (
+                    <Card
+                      link={tool.value.node.link}
+                      fixed={
+                        tool.value.node.localImage.childImageSharp
+                          ? tool.value.node.localImage.childImageSharp.fixed
+                          : null
+                      }
+                      image={tool.value.node.image}
+                      title={tool.value.node.name}
+                      text={tool.value.node.description}
+                      key={index}
+                    />
+                  ))
               : relevantData
-                .reverse()
-                .map((card, index) => (
-                  <Card
-                    link={card.node.link}
-                    fixed={card.node.localImage.childImageSharp ? card.node.localImage.childImageSharp.fixed : null}
-                    image={card.node.image}
-                    text={card.node.author}
-                    title={card.node.title}
-                    art={["art", "guides"].includes(card.node.category.toLowerCase())}
-                    key={index}
-                  />
-                ))}
+                  .reverse()
+                  .map((card, index) => (
+                    <Card
+                      link={card.node.link}
+                      fixed={
+                        card.node.localImage.childImageSharp
+                          ? card.node.localImage.childImageSharp.fixed
+                          : null
+                      }
+                      image={card.node.image}
+                      text={card.node.author}
+                      title={card.node.title}
+                      art={["art", "guides"].includes(
+                        card.node.category.toLowerCase()
+                      )}
+                      key={index}
+                    />
+                  ))}
           </div>
         </Layout>
       )
